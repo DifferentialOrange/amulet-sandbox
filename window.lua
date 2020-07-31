099ddc8e0a9be539fd448bdb7366e69ddc5e9116
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2020-07-31 21:09:37 +0300

    Play a bit

--- window.lua
+++ window.lua
@@ -1,6 +1,6 @@
-local win = am.window{
-    title = "Hi",
+am.window{
+    title = "My window",
     width = 400,
     height = 300,
-    clear_color = vec4(1, 0, 0.5, 1)
+    clear_color = vec4(0, -2, 0.5, 1)
 }
